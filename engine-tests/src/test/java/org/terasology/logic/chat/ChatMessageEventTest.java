8798baf9b0def41303089e0a7584f738035ba703
Elliott Sipple <elliott@elliotts-mbp.hh.nku.edu>
2018-10-29 17:12:27 -0400

    added tests for ChatSystem and ChatMessageEvent

---
+++ engine-tests/src/test/java/org/terasology/logic/chat/ChatMessageEventTest.java
@@ -0,0 +1,40 @@
+package org.terasology.logic.chat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.logic.console.Message;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class ChatMessageEventTest {
+    private String message;
+    @Mock
+    EntityRef er;
+
+    @Mock
+    ChatMessageEvent cme;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        message = "Hello, World!";
+    }
+
+    @Test
+    public void ifPassedMockSenderAndString_whenGetMessage_thenReturnMessage() {
+        ChatMessageEvent actual = new ChatMessageEvent(message, er);
+        assertEquals("Hello, World!", actual.getMessage());
+    }
+
+    @Test
+    public void ifPassedMockSenderAndString_whenGetFormattedMessage_thenReturnFormattedMessage() {
+        String message = "Hello, World!";
+        Message expected = new Message("Elliott: Hello, World!");
+        when(cme.getFormattedMessage()).thenReturn(expected);
+        assertEquals(expected, cme.getFormattedMessage());
+    }
+}
---
+++ engine-tests/src/test/java/org/terasology/logic/chat/ChatSystemTest.java
@@ -0,0 +1,33 @@
+package org.terasology.logic.chat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.terasology.entitySystem.entity.EntityRef;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class ChatSystemTest {
+    @Mock
+    EntityRef er;
+
+    @Mock
+    ChatSystem cs;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void ifPassedMockSenderAndString_whenSay_returnSent() {
+        ChatSystem chat = new ChatSystem();
+        String[] message = {"Hello, World!"};
+        when(cs.say(er, message)).thenReturn("Message sent.");
+
+        String actual = cs.say(er, message);
+        assertEquals("Message sent.", actual);
+    }
+}
